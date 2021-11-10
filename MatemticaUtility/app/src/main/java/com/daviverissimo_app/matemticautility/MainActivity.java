92fb89a1fa312aea2355dacc0e1ee03231109bdf
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2021-11-10 01:16:40 -0300

    add Tela Equivalência em Porcentagem
    activity Equivalência em Porcentagem e seu layout.

--- MatemticaUtility/app/src/main/java/com/daviverissimo_app/matemticautility/MainActivity.java
+++ MatemticaUtility/app/src/main/java/com/daviverissimo_app/matemticautility/MainActivity.java
@@ -9,11 +9,13 @@ import android.content.Intent;
 import com.daviverissimo_app.matemticautility.view.ActivityCalcularPorcentagem;
 import com.daviverissimo_app.matemticautility.view.ActivityCalcularRaiz;
 import com.daviverissimo_app.matemticautility.view.ActivityCalculoPotencia;
+import com.daviverissimo_app.matemticautility.view.ActivityEquivalenciaEmPorcentagem;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnCalcPotencia;
     private Button btnCalcRaiz;
     private Button btnCalcPorcentagem;
+    private Button btnCalcPorcentagemEquivalente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         btnCalcPotencia = (Button) findViewById(R.id.btnCalcPotencia);
         btnCalcRaiz = (Button) findViewById(R.id.btnCalcRaiz);
         btnCalcPorcentagem = (Button) findViewById(R.id.btnCalcPorcentagem);
+        btnCalcPorcentagemEquivalente = (Button) findViewById(R.id.btnCalcPorcentagemEquivalente);
 
         btnCalcPotencia.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +47,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ActivityCalcularPorcentagem.class);
+                startActivity(intent);
+            }
+        });
+
+        btnCalcPorcentagemEquivalente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ActivityEquivalenciaEmPorcentagem.class);
                 startActivity(intent);
             }
         });
---
+++ MatemticaUtility/app/src/main/java/com/daviverissimo_app/matemticautility/view/ActivityEquivalenciaEmPorcentagem.java
@@ -0,0 +1,42 @@
+package com.daviverissimo_app.matemticautility.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.daviverissimo_app.matemticautility.R;
+import com.daviverissimo_app.matemticautility.control.Sistema;
+
+public class ActivityEquivalenciaEmPorcentagem extends AppCompatActivity {
+    private EditText edtNumero2;
+    private EditText edtPocentagemEquivale;
+    private Button btnCalcularPorcentagem;
+    private TextView resultadoPorcentagemEquivalente;
+    private Sistema sistema;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_equivalencia_em_porcentagem);
+
+        edtNumero2 = (EditText) findViewById(R.id.edtNumero2);
+        edtPocentagemEquivale = (EditText) findViewById(R.id.edtPocentagemEquivale);
+        btnCalcularPorcentagem = (Button) findViewById(R.id.btnCalcularPorcentagem);
+        resultadoPorcentagemEquivalente = (TextView) findViewById(R.id.resultadoPorcentagemEquivalente);
+
+        btnCalcularPorcentagem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sistema = new Sistema();
+                float numero = Float.parseFloat(edtNumero2.getText().toString());
+                float porcentagem = Float.parseFloat(edtPocentagemEquivale.getText().toString());
+                float aux = sistema.calcularQuantosPorcentoDoNumero(numero, porcentagem);
+                String resultado = Float.toString(aux);
+                resultadoPorcentagemEquivalente.setText(resultado + "%");
+            }
+        });
+    }
+}
