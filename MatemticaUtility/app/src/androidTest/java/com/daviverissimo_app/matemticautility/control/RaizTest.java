a2b4a6bf5048d26ec90e4d10199b02f6f8c9efed
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2021-11-12 16:25:23 -0300

    add testes calculo raiz
    testes de unidade para calculo de raiz e sobrescrita de método no controller para suporte parcial a long.

---
+++ MatemticaUtility/app/src/androidTest/java/com/daviverissimo_app/matemticautility/control/RaizTest.java
@@ -0,0 +1,73 @@
+package com.daviverissimo_app.matemticautility.control;
+
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RaizTest {
+
+    @Test
+    public void calcularRaizParExpoentImpar() {
+        Sistema sistema = new Sistema();
+        float raiz = 2;
+        float expoent = 9;
+        long resultado = sistema.calcularRaiz(expoent, raiz);
+        Assert.assertEquals(resultado, 3);
+    }
+
+    @Test
+    public void calcularRaizImparExpoentPar(){
+        Sistema sistema = new Sistema();
+        float raiz =  3;
+        float expoent = 512;
+        long resultado = sistema.calcularRaiz(expoent, raiz);
+        Assert.assertEquals(resultado, 8);
+    }
+
+    @Test
+    public void calcularRaizImparExpoenteImpar(){
+        Sistema sistema = new Sistema();
+        float raiz = 9;
+        float expoent = 40353607;
+        long resultado = sistema.calcularRaiz(expoent, raiz);
+        Assert.assertEquals(resultado, 7);
+    }
+
+    @Test
+    public void calcularRaizParExpoentPar(){
+        Sistema sistema = new Sistema();
+        float raiz = 12;
+        float expoent = 16777216;
+        long resultado = sistema.calcularRaiz(expoent, raiz);
+        Assert.assertEquals(resultado, 4);
+
+    }
+
+    @Test
+    public void calcularRaizPrimaExpoentPrimo(){
+        Sistema sistema = new Sistema();
+        float raiz = 7;
+        float expoent = 62748517;
+        long resultado = sistema.calcularRaiz(expoent, raiz);
+        Assert.assertEquals(resultado, 13);
+    }
+
+    @Test
+    public void calcularRaizMultiploDeCinco(){
+        Sistema sistema = new Sistema();
+        float raiz = 5;
+        float expoent = 9765625;
+        long resultado = sistema.calcularRaiz(expoent, raiz);
+        Assert.assertEquals(resultado, 25);
+    }
+
+    @Test
+    public void calcularRaizParametrosLong(){
+        Sistema sistema = new Sistema();
+        long raiz = 12;
+        long expoent = 1000000000000l;
+        long resultado = sistema.calcularRaiz(expoent, raiz);
+        long esperado = 10;
+        Assert.assertEquals(resultado, esperado);
+    }
+}
--- MatemticaUtility/app/src/main/java/com/daviverissimo_app/matemticautility/model/Calculadora.java
+++ MatemticaUtility/app/src/main/java/com/daviverissimo_app/matemticautility/model/Calculadora.java
@@ -6,6 +6,11 @@ public class Calculadora {
 		long raiz = (long) Math.pow(numero, 1 / baseRaiz);
 		return raiz;
 	}
+
+	public long calcularRaiz(long numero, long baseRaiz) {
+		long raiz = (long) Math.pow(numero, 1 / baseRaiz);
+		return raiz;
+	}
 	
 	public long calcularPotentia(double numero, double basePotencia) {
 		Double potenciacao = Math.pow(numero, basePotencia);
